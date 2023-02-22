24a42d68dd9e25153fb42ea9973e0528c311f42e
john <johnny.lioup@gmail.com>
2023-02-22 15:42:12 +0000

    added examplar for core and extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -28,7 +28,7 @@ public class Extension {
 
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        sb.append("Hello, world!");
 
 
         // ...AND THIS LINE
@@ -42,7 +42,8 @@ public class Extension {
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // 2. After adding the message, use an appropriate StringBuilder method to reverse it
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        sb.append("Hello, world!");
+        sb.reverse();
 
 
         // ...AND THIS LINE
@@ -56,7 +57,9 @@ public class Extension {
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // 2. After adding the message, remove the comma.
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        sb.append("Hello, world!");
+        int commaIndex = sb.indexOf(",");
+        sb.deleteCharAt(commaIndex);
 
 
         // ...AND THIS LINE
@@ -70,7 +73,10 @@ public class Extension {
         // 1. Using the sb variable above, add "Hello, world!" to the StringBuilder
         // 2. After adding the message, replace the word "world" with the word "Java"
         // WRITE YOUR CODE BETWEEN THIS LINE...
-
+        sb.append("Hello, world!");
+        int worldIndex = sb.indexOf("world");
+        int worldIndexEnd = sb.indexOf("!");
+        sb.replace(worldIndex, worldIndexEnd, "Java");
 
 
         // ...AND THIS LINE
